ebafd99777489d5ca2b9bd3fb41b14acb9059180
Piotr Pospiech <ptkpospiech@gmail.com>
2020-02-18 15:23:23 +0100

    created shop and cart components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from '../Navbar';
+import Shop from '../Shop';
+import Cart from '../Cart';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <Route exact path='/' />
-      <Route path='/cart' />
+      <Route exact path='/' component={Shop} />
+      <Route path='/cart' component={Cart} />
     </Router>
   );
 };
