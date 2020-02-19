f6448eaab75089d889fb5e7e00c900d7d1a91544
Piotr Pospiech <ptkpospiech@gmail.com>
2020-02-19 02:44:12 +0100

    fixed rwd

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { withRouter } from "react-router";
 import { connect } from 'react-redux';
-import { Container, Grid, Image, Button } from 'semantic-ui-react';
+import { Container, Grid, Image, Button, Responsive } from 'semantic-ui-react';
 
 import { fetchProductBySlug, addToCart } from '../../store/actions';
 
@@ -29,29 +29,47 @@ class Details extends Component {
     });
   };
 
-  render() {
+  renderProductText = (textAlign, addPriceMargin) => {
 
     const { isAdded } = this.state;
-    const { title, slug, author, description, price, image } = this.props.product;
+    const { title, author, description, price, image } = this.props.product;
 
+    const marginRight = addPriceMargin ? '10px' : '0px';
     const buttonText = isAdded ? 'Added!' : 'Add to cart';
+
+    return (
+      <Fragment>
+        <Grid.Column>
+          <Image src={image} />
+        </Grid.Column>
+        <Grid.Column textAlign={textAlign}>
+          <h3>{title}</h3>
+          <h3 style={{ fontSize: '1rem', marginTop: 0 }}>{author}</h3>
+          <p>{description}</p>
+          <h4 style={{ fontSize: '1.5rem', marginRight: {marginRight} }}>${price}</h4>
+          <Button primary onClick={this.handleCartButton}>{buttonText}</Button>
+        </Grid.Column>
+      </Fragment>
+    );
+  };
+
+  render() {
+
+    const { title } = this.props.product;
 
     return (
       <main>
         <PageTitle title={title} leftColor='#343144' rightColor='#4ebbd2' />
         <Container style={{ marginTop: '20px' }}>
-          <Grid columns={2}>
-            <Grid.Column>
-              <Image src={image} />
-            </Grid.Column>
-            <Grid.Column>
-              <h3>{title}</h3>
-              <h3 style={{ fontSize: '1rem', marginTop: 0 }}>{author}</h3>
-              <p>{description}</p>
-              <h4 style={{ fontSize: '1.5rem' }}>${price}</h4>
-              <Button primary onClick={this.handleCartButton}>{buttonText}</Button>
-            </Grid.Column>
-          </Grid>
+          <Responsive as={Grid} columns={1} {...Responsive.onlyMobile}>
+            {this.renderProductText('center', true)}
+          </Responsive>
+          <Responsive as={Grid} columns={2} {...Responsive.onlyTablet}>
+            {this.renderProductText('left', false)}
+          </Responsive>
+          <Responsive as={Grid} columns={2} {...Responsive.onlyComputer}>
+            {this.renderProductText('left', false)}
+          </Responsive>
         </Container>
       </main>
     );
