8e5d9d55f66b8de59d198e8bcf54a357f8affd1b
Piotr Pospiech <ptkpospiech@gmail.com>
2020-02-25 16:13:19 +0100

    created dashboard layout

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Menu, Input, Segment, Container } from 'semantic-ui-react';
+
+import PageTitle from '../PageTitle/PageTitle';
 
 const Dashboard = () => {
   return (
-    <div></div>
+    <main>
+      <PageTitle title='dashboard' leftColor='#343144' rightColor='#343144' />
+      <Container style={{ marginTop: '20px' }}>
+        <Menu>
+          <Menu.Item
+            name='orders'
+          />
+          <Menu.Item
+            name='completed'
+          />
+        </Menu>
+
+        {/* <OrdersList /> */}
+      </Container>
+    </main>
   );
 };
 
