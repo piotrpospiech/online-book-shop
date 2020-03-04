5cd8420e7cd492f04542834a8d82c10bee5dee2e
Piotr Pospiech <ptkpospiech@gmail.com>
2020-03-04 23:21:44 +0100

    removed active flag from logo

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { Segment, Image, Dimmer, Icon, Button } from 'semantic-ui-react';
+import { Segment, Image, Dimmer, Icon, Button, Responsive } from 'semantic-ui-react';
 
 class ProductCard extends Component {
 
@@ -13,7 +13,7 @@ class ProductCard extends Component {
   handleShow = () => this.setState({ active: true });
   handleHide = () => this.setState({ active: false });
 
-  render() {
+  renderCard = () => {
 
     const { active } = this.state;
     const { image, title, slug, author, price } = this.props.product;
@@ -27,6 +27,7 @@ class ProductCard extends Component {
         <Icon size='big' name='search' />
         <br />
         <Button 
+          color='teal'
           as={Link} 
           to={goTo}
           style={{ marginTop: '20px' }}
@@ -39,13 +40,13 @@ class ProductCard extends Component {
     return (
       <article>
         <Dimmer.Dimmable 
-        as={Image} 
-        src={image} 
-        dimmed={active}
-        blurring
-        dimmer={{ active, content }}
-        onMouseEnter={this.handleShow}
-        onMouseLeave={this.handleHide}
+          as={Image} 
+          src={image} 
+          dimmed={active}
+          blurring
+          dimmer={{ active, content }}
+          onMouseEnter={this.handleShow}
+          onMouseLeave={this.handleHide}
         />
         <Segment>
           <h3 style={{ margin: 0 }}>{title}</h3>
@@ -53,6 +54,55 @@ class ProductCard extends Component {
           <span>${price}</span>
         </Segment>
       </article>
+    )
+  };
+
+  renderMobileCard = () => {
+
+    const { image, title, slug, author, price } = this.props.product;
+    const { admin } = this.props;
+
+    const buttonText = admin ? 'Edit' : 'Details';
+    const goTo = admin ? `edit/${slug}` : `details/${slug}`;
+
+    return (
+      <article>
+        <Image 
+          src={image} 
+          onMouseEnter={this.handleShow}
+          onMouseLeave={this.handleHide}
+        />
+        <Segment>
+          <h3 style={{ margin: 0 }}>{title}</h3>
+          <h5 style={{ marginTop: 0 }}>{author}</h5>
+          <span>${price}</span><br />
+          <Button 
+            color='teal'
+            style={{ marginTop: 20 }}
+            as={Link} 
+            to={goTo}
+          >
+            {buttonText}
+          </Button>
+        </Segment>
+      </article>
+    )
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Responsive {...Responsive.onlyMobile}>
+          {this.renderMobileCard()}
+        </Responsive>
+        <Responsive {...Responsive.onlyTablet}>
+          {this.renderCard()}
+        </Responsive>
+        <Responsive {...Responsive.onlyComputer}>
+          {this.renderCard()}
+        </Responsive>
+       
+      </Fragment>
     );
   }
 }
