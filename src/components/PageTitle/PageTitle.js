028a66123c12d51d2bfeac07d895281ed4b75ece
Piotr Pospiech <ptkpospiech@gmail.com>
2020-03-06 18:16:45 +0100

    fixed page title display

--- src/components/PageTitle/PageTitle.js
+++ src/components/PageTitle/PageTitle.js
@@ -11,7 +11,8 @@ const PageTitle = (props) => {
     top: '50%',
     transform: 'translate(-50%, -50%)',
     color: '#fff',
-    textTransform: 'uppercase'
+    textTransform: 'uppercase',
+    textAlign: 'center'
   }
 
   return (
